ea6220a47c233dc2d98874ee3247190953858c6e
benjaminadk <benjaminadk@gmail.com>
2019-03-08 10:27:25 -0800

    :bug: Fix database managment key

--- backend/src/generated/index.js
+++ backend/src/generated/index.js
@@ -40,7 +40,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `${process.env["PRISMA_ENDPOINT_DEV"]}`,
+  endpoint: `${process.env["PRISMA_ENDPOINT_PROD"]}`,
   secret: `${process.env["PRISMA_SECRET"]}`
 });
 exports.prisma = new exports.Prisma();
