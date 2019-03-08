8e1e060311f69ae7980bd1f3c4e4bf05be00dfcd
benjaminadk <benjaminadk@gmail.com>
2019-03-08 09:33:58 -0800

    :rocket: Deploy frontend code to Heroku

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,7 @@
-export const endpointHttp = 'http://localhost:8081/graphql'
-export const endpointWs = 'ws://localhost:8081/graphql'
-export const frontend = 'http://localhost:8080'
+export const endpointHttp =
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8081/graphql'
+export const endpointWs = process.env.NODE_ENV === 'production' ? '' : 'ws://localhost:8081/graphql'
+export const frontend = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080'
 export const googleClientID =
   '162812165284-hvi6iqeqtatt48d2cv824d3bl4n9b5ig.apps.googleusercontent.com'
 export const logoURL = 'https://s3.amazonaws.com/electron-exam/general/icon-red.png'
